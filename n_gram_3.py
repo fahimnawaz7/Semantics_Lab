787bfc3969c8144f6eb6427df7425d365d68983f
fahimnawaz7 <53517887+fahimnawaz7@users.noreply.github.com>
2019-10-14 11:39:44 +0600

    Add files via upload

---
+++ n_gram_3.py
@@ -0,0 +1,34 @@
+#141019@1102
+# A program to find upto n-grams(here 3 grams) of each line from a text file.
+import re
+import nltk
+d = {}
+with open ("n_gram.txt", "r") as fileHandler:
+    for line in fileHandler:
+	    s = line.lower()
+	    s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
+	    tokens = nltk.word_tokenize(s)
+		
+	    for i in range(len(tokens)):
+
+		    if tokens[i] in d:
+			    d[tokens[i]] += 1
+		    else:
+			    d[tokens[i]] = 1
+
+		    if i < len(tokens) - 1:
+			    two_gram = tokens[i] + ' ' + tokens[i + 1]
+			    if two_gram in d:
+				    d[two_gram] += 1
+			    else:
+				    d[two_gram] = 1
+
+		    if i < len(tokens) - 2:
+			    three_gram = tokens[i] + ' ' + tokens[i + 1] + ' ' + tokens[i + 2]
+			    if three_gram in d:
+				    d[three_gram] += 1
+			    else:
+				    d[three_gram] = 1
+
+for key, value in d.items():
+	print(key, value)
